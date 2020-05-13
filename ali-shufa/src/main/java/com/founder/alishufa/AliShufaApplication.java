7afb519a6f2203666e624e06bead9007fb200252
lhp <wolf88840@163.com>
2020-05-13 16:59:08 +0800

    初始化

---
+++ ali-shufa/src/main/java/com/founder/alishufa/AliShufaApplication.java
@@ -0,0 +1,13 @@
+package com.founder.alishufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliShufaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliShufaApplication.class, args);
+    }
+
+}
---
+++ ali-shufa/src/test/java/com/founder/alishufa/AliShufaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.founder.alishufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliShufaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
